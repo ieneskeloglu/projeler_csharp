792f6ce6eb41ac5075fd823b2780e2363b995a21
İhsan Enes <ieneskeloglu2@outlook.com>
2022-11-17 03:46:39 +0300

    karakter_tersten_yazdırma

---
+++ karakter_tersten_yazdırma/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace karakter_tersten_yazdırma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cümle giriniz: ");
+            string cumle=Console.ReadLine();
+            KelimeCevir(cumle);
+            Console.ReadKey();
+        }
+        public static void KelimeCevir(string cumle){
+            string[] Array=cumle.Split(' ');
+            foreach (var item in Array)
+            {
+                for(int i=item.Length-1;i>=0;i--){
+                    Console.Write(item[i]);
+                }
+                Console.Write(" ");
+            }
+        }
+    }
+}
